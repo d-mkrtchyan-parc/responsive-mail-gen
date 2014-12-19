692db5f18fd4204a971d7d057263aa3bcc4bed42
Dat Mkrtchyan <d.mkrtchyan@parcsis.org>
2014-12-19 18:28:44 +0400

    Исправил ошибки с пустыми атрибутами. Добавил возможность редактировать текст в приветствии. Подправил разметку.

--- js/main.js
+++ js/main.js
@@ -1,100 +1,9 @@
 $(function(){
-	function exist(x){
-		return typeof x != "undefined" && x !== null;
-	}
-
-	function replace(o, c){
-		if(c=='1'){
-			o.html(o.html().replace('Акция', 'Акции').replace('Премьера', 'Премьеры'));
-		}else{
-			o.html(o.html().replace('Акции', 'Акция').replace('Премьеры', 'Премьера'));
-		}
-	}
-
-	function getData(item){
-		return ({
-			actions0 : {
-				width : 192,
-				height : 35,
-				number : 5,
-				alt : 'Акция недели',
-				name : 'Акция недели'
-			},
-			actions1 : {
-				width : 192,
-				height : 35,
-				number : 7,
-				alt : 'Акции недели',
-				name : 'Акции недели'
-			},			
-			news : {
-				width: 232,
-				height: 36,
-				number: 1,
-				alt: 'Новинки недели',
-				name: 'Новинки недели'
-
-			},
-			premiers0 : {
-				width: 224,
-				height: 36,
-				number: 2,
-				alt: 'Премьера недели',
-				name: 'Премьера недели'
-			},
-			premiers1 : {
-				width: 228,
-				height: 35,
-				number: 6,
-				alt: 'Премьеры недели',
-				name: 'Премьеры недели'
-			},
-			bestsellers : {
-				width: 256,
-				height: 35,
-				number : 4,
-				alt: 'Бестселлеры недели',
-				name: 'Бестселлеры недели'
-			}
-		})[item];		
-	}
-
-	function interpolate(str){
-      var data = {},
-        argc = arguments.length,
-        argv = Array.prototype.slice.call(arguments),
-        reg = /{{\s*[\w\.]+\s*}}/g;
-
-      if(argc==2 && typeof argv[1] === 'object'){
-        data = argv[1];
-      }else{
-        argv.slice(1, argc).forEach(function(e, i){
-          data[i] = e;
-        });
-      }       
-
-      return str.replace(reg, function(i){
-        var arg = data[i.slice(2,-2)] || i;
-        if(typeof arg === 'object'){
-          arg=JSON.stringify(arg);
-        }
-        return arg;
-      });
-    }
-
-    function uuid(){
-    	return [1,2,3].map(function(){ return (Math.random()*1000)>>0}).join('-');
-    }
-
 	var list = $(".gen-content ul#sortable"),
 		table = $("table.main"),
 		anchor = $("#replacement"),
 		cached = $(".gen-demo").html(),
 		conjunctions = {};
-
-
-
-
 
 	list.sortable();
 
@@ -109,7 +18,6 @@ $(function(){
 			$(this).append("<span class='ball quantity'></span>")
 		}
 	});
-
 
 	conjunctions['default'] = {
 		urlimg : {
@@ -152,6 +60,9 @@ $(function(){
 		item.after(item.clone());
 		item.next().removeClass('dirty').removeAttr('conjuncted');
 	}).
+
+
+
 	delegate('.gen-item .remove', 'click', function(event){
 		var item = $(this).parent(),
 			isRepeatable = exist(item.attr('data-repeatable')),
@@ -165,6 +76,9 @@ $(function(){
 			item.toggleClass('disabled');
 		}
 	}).
+
+
+
 	delegate('.gen-item .quantity', 'click', function(event){
 		var item = $(this).parent();
 
@@ -178,6 +92,9 @@ $(function(){
 
 		replace(item, item.attr('plural'));
 	}).
+
+
+
 	delegate('.gen-item a:not(#save):not(#cancle)', 'click', function(e){
 		e.preventDefault();
 		var $this = $(this),
@@ -200,6 +117,7 @@ $(function(){
 				$('.params form input, .params form select, .params form textarea').each(function(){
 					var val = conjunctions[id] && conjunctions[id][$(this).attr('name')];
 					$(this).val(val || "");
+					conjunctions[id] = val; /* Check out */
 				});
 			}
 		}else{
@@ -207,7 +125,10 @@ $(function(){
 		}
 
 		$this.toggleClass('open');		
-	}).delegate('.gen-item a#save', 'click', function(e){
+	}).
+
+
+	delegate('.gen-item a#save', 'click', function(e){
 		e.preventDefault();
 
 		var data = {},
@@ -222,10 +143,16 @@ $(function(){
 		$('.gen-item a.open').removeClass('open');
 		$('.params').remove();
 	}).
+	
+
 	delegate('.gen-item a#cancle', 'click', function(e){
 		e.preventDefault();
 		$('.params').remove();
 	});
+
+
+	/* Buttons */
+
 
 	$("#width").change(function(){
 		var cw = table.width(),
---
+++ js/utils.js
@@ -0,0 +1,86 @@
+function exist(x){
+	return typeof x != "undefined" && x !== null;
+}
+
+function replace(o, c){
+	if(c=='1'){
+		o.html(o.html().replace('Акция', 'Акции').replace('Премьера', 'Премьеры'));
+	}else{
+		o.html(o.html().replace('Акции', 'Акция').replace('Премьеры', 'Премьера'));
+	}
+}
+
+function getData(item){
+	return ({
+		actions0 : {
+			width : 192,
+			height : 35,
+			number : 5,
+			alt : 'Акция недели',
+			name : 'Акция недели'
+		},
+		actions1 : {
+			width : 192,
+			height : 35,
+			number : 7,
+			alt : 'Акции недели',
+			name : 'Акции недели'
+		},			
+		news : {
+			width: 232,
+			height: 36,
+			number: 1,
+			alt: 'Новинки недели',
+			name: 'Новинки недели'
+
+		},
+		premiers0 : {
+			width: 224,
+			height: 36,
+			number: 2,
+			alt: 'Премьера недели',
+			name: 'Премьера недели'
+		},
+		premiers1 : {
+			width: 228,
+			height: 35,
+			number: 6,
+			alt: 'Премьеры недели',
+			name: 'Премьеры недели'
+		},
+		bestsellers : {
+			width: 256,
+			height: 35,
+			number : 4,
+			alt: 'Бестселлеры недели',
+			name: 'Бестселлеры недели'
+		}
+	})[item];		
+}
+
+function interpolate(str){
+  var data = {},
+    argc = arguments.length,
+    argv = Array.prototype.slice.call(arguments),
+    reg = /{{\s*[\w\.]+\s*}}/g;
+
+  if(argc==2 && typeof argv[1] === 'object'){
+    data = argv[1];
+  }else{
+    argv.slice(1, argc).forEach(function(e, i){
+      data[i] = e;
+    });
+  }       
+
+  return str.replace(reg, function(i){
+    var arg = data[i.slice(2,-2)] || "";
+    if(typeof arg === 'object'){
+      arg=JSON.stringify(arg);
+    }
+    return arg;
+  });
+}
+
+function uuid(){
+	return [1,2,3].map(function(){ return (Math.random()*1000)>>0}).join('-');
+}
